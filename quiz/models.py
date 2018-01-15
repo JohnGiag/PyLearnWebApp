a7a01f3a59920472151dd76ecec0f2dd397560f5
JohnGiag <it1257@uom.edu.gr>
2018-01-15 15:40:54 +0200

    order

--- quiz/models.py
+++ quiz/models.py
@@ -1,43 +1,31 @@
-from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
-from django.forms import ModelForm
 
 
 
 
 class Quiz(models.Model):
-    name = models.CharField(max_length=120,default="")
-    questions = models.ManyToManyField('Question',related_name='question')
-
-
+    order = models.IntegerField(null=True)
+    name = models.CharField(max_length=120, default="")
+    questions = models.ManyToManyField('Question', related_name='question')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name='Quiz'
-        verbose_name_plural='Quizes'
+        verbose_name = 'Quiz'
+        verbose_name_plural = 'Quizes'
+        ordering = ['order']
 
 
 class Question(models.Model):
-
-    question_text=models.CharField(max_length=255)
-    answer1= models.CharField(max_length=255)
-    answer2= models.CharField(max_length=255)
-    answer3= models.CharField(max_length=255)
-    correct_answer =models.CharField(max_length=255)
-
+    question_text = models.CharField(max_length=255)
+    answer1 = models.CharField(max_length=255)
+    answer2 = models.CharField(max_length=255)
+    answer3 = models.CharField(max_length=255)
+    correct_answer = models.CharField(max_length=255)
 
     def __str__(self):
-        return self. question_text
-
-
-
-
-
-
-
-
-
+        return self.question_text
